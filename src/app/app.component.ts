3bddb13342cf32615028428575843c99821bfe78
austin <business.mocksock@gmail.com>
2022-06-20 16:59:56 -0500

    notes on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // This is the name of the tag
   selector: 'app-root',
+  // This is the route to a tag that 
+  // gives a structure to the file via HTML
   templateUrl: './app.component.html',
+  // This is the route 
+  // to the style sheet
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
