b87638404f55ca7131538a00bd1f9dc56ecf8a56
austin <business.mocksock@gmail.com>
2022-06-21 10:58:40 -0500

    Add method and method notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,6 @@ export class AppComponent {
   // Angular Core is the basic features that 
   // are provided, and come as a host of things. 
 
-  // The first is Templates:
-
-  // Let's show it off with an input tag
-
   // Why is it so good?
 
   data = {
@@ -25,6 +21,16 @@ export class AppComponent {
   // in order to keep the code organized
   // But importantly, the view is now requesting
   // soemthing for the model to do
+
+  // Can it modify things in real time though? 
+  // Yes! Let's change the data.title value 
+
+  // This is called using (keyup) in the tag and a
+  // template reference using # 
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle
+  }
 
   soundAlarm() {
     alert("Why did you click this? I thought I hid it well!")
