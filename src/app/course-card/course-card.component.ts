e7a14ef428164287d79a76478036d61dfeee08a1
austin <business.mocksock@gmail.com>
2022-06-21 11:48:01 -0500

    begin start of next portion of the project

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -5,10 +5,12 @@ import { Component, OnInit } from '@angular/core';
   This is especially strong, since the code can 
   be set out to multiple templates, all with their own
   code that can be ran at any time, and updated 
-  independently from each other
+  independently from each other. Most of all this can 
+  be done quickly, speeding up the development cycle
 */
 
 @Component({
+  // Notice that the name can be changed, but shouldn't be done
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
