75a474464c5b7a0f796702d7341787cf9a33c642
austin <business.mocksock@gmail.com>
2022-06-21 11:42:29 -0500

    add notes for generation

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+  This is created from ng generate component ______
+  This is especially strong, since the code can 
+  be set out to multiple templates, all with their own
+  code that can be ran at any time, and updated 
+  independently from each other
+*/
+
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
