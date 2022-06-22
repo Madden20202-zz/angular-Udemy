d55fba10f13126f876e3ad65d2d57ebb8406af92
austin <business.mocksock@gmail.com>
2022-06-22 05:16:08 -0500

    add notes and then make variable for the data

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Notes } from 'src/environments/db-data';
 
 @Component({
   selector: 'app-note-card',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent implements OnInit {
+
+  /*
+    In order for any information to be used, 
+    it has to be passed down all layers. Let's 
+    explore it with a note taking app
+
+    First, a variable has to be made that will 
+    contain the information
+  */
+
+    dataCard = Notes[0]; 
 
   constructor() { }
 
