2d864160133f9d347bdaf30f1b5a750394413a5c
austin <business.mocksock@gmail.com>
2022-06-22 05:52:46 -0500

    prep anotehr portion

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -16,6 +16,12 @@ export class NoteCardComponent implements OnInit {
     First, a variable has to be made that will 
     contain the information. but with the current
     lesson we will make one fo reach note
+
+    Now all these will match up in the HTML file
+    but will be hard coded in and what is not 
+    added will just not be shown
+
+    What if we wanna pass it as an input?
   */
 
     groceryList = Notes[0]
