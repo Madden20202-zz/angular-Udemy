e6b3db495110e6aae7825a8c9131d5acbc0b6827
austin <business.mocksock@gmail.com>
2022-06-28 09:49:17 -0500

    notes for next session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,22 +23,24 @@ export class AppComponent {
   groceryList = Notes[0]
   phoneCard = Notes[1]
 
-  // Now the method will go under everything
-  // in order to keep the code organized
-  // But importantly, the view is now requesting
-  // soemthing for the model to do
+  /*
+    Now the method will go under everything
+    in order to keep the code organized
+    But importantly, the view is now requesting
+    soemthing for the model to do
 
-  // Can it modify things in real time though? 
-  // Yes! Let's change the data.title value 
+    Can it modify things in real time though? 
+    Yes! Let's change the data.title value 
 
-  // This is called using (keyup) in the tag and a
-  // template reference using # 
+    This is called using (keyup) in the tag and a
+    template reference using # 
 
-  // When the data is modified, the model updates, 
-  // which is then reflected into the view. Notice 
-  // that the whole process did not require any logic
-  // to be implimented to start it off. This just is done
-  // on its own if done right!!
+    When the data is modified, the model updates, 
+    which is then reflected into the view. Notice 
+    that the whole process did not require any logic
+    to be implimented to start it off. This just is done
+    on its own if done right!!
+  */
 
   onKeyUp(newTitle: string) {
     this.data.title = newTitle
