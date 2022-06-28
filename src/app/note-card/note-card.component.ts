10d5a85ff56a672e69242b13b9ee6b8341243d87
austin <business.mocksock@gmail.com>
2022-06-28 10:18:13 -0500

    add a model and clean up code

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Note } from './note-card.model';
 
 @Component({
   selector: 'app-note-card',
@@ -12,13 +13,8 @@ export class NoteCardComponent implements OnInit {
     it has to be passed down all layers. Let's 
     explore it with a note taking app
 
-    First, a variable has to be made that will 
-    contain the information. but with the current
-    lesson we will make one fo reach note
-
-    Now all these will match up in the HTML file
-    but will be hard coded in and what is not 
-    added will just not be shown
+    Amended, a model file is better since it 
+    cleans up the code and makes it more automated
 
     What if we wanna pass it as an input?
     All that has to be done is add a variable that 
@@ -26,8 +22,7 @@ export class NoteCardComponent implements OnInit {
     input from somewhere and use it later
   */
 
-    @Input() title?: string
-    @Input() content?: string
+    @Input() noteCard!: Note
 
   constructor() { }
 
